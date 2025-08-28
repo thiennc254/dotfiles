0b0660c80320fccc78c70033b821e3fab73ee1ff
Nguyen Chi Thien <thiennc254.work@gmail.com>
2025-08-29 00:43:13 +0700

    Update

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,17 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
+-- [Select all]
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- [Tmux - Vim Navigator]
+keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", opts)
+keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", opts)
+keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", opts)
+keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", opts)
+
+-- [Highlight]
+keymap.set("n", "<leader>h", ":nohlsearch<CR>", opts)
+
+-- [Command mode]
+keymap.set("n", ";", ":")
