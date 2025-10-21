6d2e8ebc995619b667bbfea6e7dc8261c819315b
Nguyen Chi Thien <thiennc254.work@gmail.com>
2025-10-21 08:26:02 +0700

    new nvim update

--- nvim/.config/nvim/lua/plugins/all-theme.lua
+++ nvim/.config/nvim/lua/plugins/all-theme.lua
@@ -66,9 +66,24 @@ return {
         },
     },
     {
-        "LazyVim/LazyVim",
+        "folke/tokyonight.nvim",
         opts = {
-            colorscheme = "catppuccin-mocha",
+            transparent = true,
+            -- dim_inactive = true,
+            lualine_bold = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+            on_colors = function(colors)
+                colors.bg_statusline = colors.none
+            end,
+            on_highlights = function(hl, opts)
+                hl.TabLineFill = { bg = opts.transparent }
+                hl.LineNrAbove = { fg = "#c0caf5", bold = false }
+                hl.LineNrBelow = { fg = "#c0caf5", bold = false }
+                -- hl.LineNr = { fg = "white", bold = true }
+            end,
         },
     },
 }
