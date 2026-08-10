b9d81e9537395519678cf22589ec1a922a0faa7d
thiennc254 <thiennc254.work@gmail.com>
2026-08-11 02:52:24 +0700

    starship format

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -7,56 +7,76 @@ _: {
       palette = "catppuccin_mocha";
 
       format = ''
-         $username$hostname$directory$git_branch$git_status$c$cpp$golang$nodejs$python$rust$nix_shell$docker_context$cmd_duration$time
-        $character'';
+        [╭─](bold fg:overlay)$os$username$hostname $directory$git_branch$git_status$c$cpp$golang$nodejs$python$rust$nix_shell$aws$terraform$docker_context
+        [╰─](bold fg:overlay)$character
+      '';
 
       character = {
-        success_symbol = " [➜](fg:blue)";
-        error_symbol = " [➜](bold fg:red)";
-        vimcmd_symbol = " [➜](bold fg:green)";
-        vimcmd_replace_symbol = " [](bold fg:green)";
-        vimcmd_replace_one_symbol = " [](bold fg:purple)";
-        vimcmd_visual_symbol = " [](bold fg:yellow)";
+        # "➜" "❯" "►"
+        success_symbol = "[►](fg:overlay)";
+        error_symbol = "[►](bold fg:red)";
+        vimcmd_symbol = "[►](bold fg:green)";
+        vimcmd_replace_symbol = "[](bold fg:green)";
+        vimcmd_replace_one_symbol = "[](bold fg:purple)";
+        vimcmd_visual_symbol = "[](bold fg:yellow)";
       };
 
       os = {
-        disabled = false;
+        disabled = true;
         symbols = {
           Arch = " ";
+          NixOS = " ";
         };
         style = "bold fg:blue";
         format = "[$symbol]($style)";
       };
 
       username = {
+        disabled = false;
         show_always = true;
         style_user = "bold fg:red";
         format = "[$user]($style)";
       };
 
       hostname = {
+        disabled = false;
         ssh_only = true;
         style = "fg:blue";
         format = "@[$hostname]($style)";
       };
 
       directory = {
+        disabled = false;
         style = "bold fg:cyan";
-        format = " [$path]($style)";
+        format = "[$path]($style)";
         truncate_to_repo = true;
         truncation_length = 3;
         truncation_symbol = "…/";
       };
 
       git_branch = {
+        disabled = false;
         symbol = "󰘬 ";
         style = "fg:green";
         format = " [$symbol$branch]($style) ";
       };
 
       git_status = {
+        disabled = false;
         style = "fg:yellow";
         format = "([\\[$all_status$ahead_behind\\]]($style))";
+      };
+
+      git_state = {
+        disabled = false;
+        style = "bold fg:red";
+        format = " \\([$state( $progress_current/$progress_total)]($style)\\) ";
+      };
+
+      git_commit = {
+        disabled = true;
+        commit_hash_length = 7;
+        style = "fg:overlay";
       };
 
       nix_shell = {
@@ -69,51 +89,83 @@ _: {
       };
 
       golang = {
+        disabled = false;
         symbol = " ";
         style = "fg:cyan";
         format = " [$symbol$version]($style)";
       };
 
       nodejs = {
+        disabled = false;
         symbol = " ";
         style = "fg:green";
         format = " [$symbol$version]($style)";
       };
 
       python = {
+        disabled = false;
         symbol = " ";
         style = "fg:yellow";
         format = " [$symbol$version]($style)";
       };
 
       rust = {
+        disabled = false;
         symbol = " ";
         style = "fg:orange";
         format = " [$symbol$version]($style)";
-        disabled = false;
       };
 
       c = {
+        disabled = false;
         symbol = " ";
         style = "fg:purple";
         format = " [$symbol$version]($style)";
       };
 
       cpp = {
+        disabled = false;
         symbol = " ";
         style = "fg:purple";
         format = " [$symbol$version]($style)";
       };
 
       docker_context = {
+        disabled = false;
         symbol = " ";
         style = "fg:blue";
         format = " [$symbol$context]($style)";
       };
 
+      aws = {
+        disabled = true;
+        symbol = "  ";
+        style = "bold fg:orange";
+      };
+
+      terraform = {
+        disabled = true;
+        symbol = "󱁢 ";
+        style = "bold fg:purple";
+      };
+
       cmd_duration = {
+        disabled = false;
         min_time = 2000;
         format = " [⏱ $duration](bold fg:magenta)";
+      };
+
+      status = {
+        disabled = false;
+        format = " [✘ $status](bold fg:red)";
+        map_symbol = true;
+      };
+
+      memory_usage = {
+        disabled = true;
+        threshold = -1;
+        symbol = "󰍛 ";
+        style = "bold dimmed fg:green";
       };
 
       time = {
@@ -123,7 +175,6 @@ _: {
         format = " [$time]($style)";
       };
 
-      # Palettes
       palettes.catppuccin_mocha = {
         bg = "#1e1e2e";
         fg = "#cdd6f4";
