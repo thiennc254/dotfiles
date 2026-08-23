f28665cef49629ecab411783ad47d4e0562d4ee4
thiennc254 <thiennc254.work@gmail.com>
2026-08-23 07:49:25 +0700

    geist font

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -2,6 +2,8 @@
   fonts.packages = with pkgs; [
     nerd-fonts.caskaydia-cove
     nerd-fonts.jetbrains-mono
+    nerd-fonts.roboto-mono
+    geist-font
 
     noto-fonts
     noto-fonts-color-emoji
